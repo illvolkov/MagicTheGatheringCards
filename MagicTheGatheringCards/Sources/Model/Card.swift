f39fa44bbef13d5eb215c05eeae404d2aa974565
Ilya Volkov <illvolkov@bk.ru>
2022-06-28 23:17:08 +0500

    Creating a data model

---
+++ MagicTheGatheringCards/Sources/Model/Card.swift
@@ -0,0 +1,22 @@
+//
+//  Card.swift
+//  MagicTheGatheringCards
+//
+//  Created by Ilya Volkov on 28.06.2022.
+//
+
+import Foundation
+
+struct Card: Decodable {
+    let name: String
+    let manaCost: String
+    let type: String
+    let rarity: String
+    let setName: String
+    let text: String
+    let artist: String
+    let power: String
+    let toughness: String
+    let imageUrl: String
+    let flavor: String?
+}
